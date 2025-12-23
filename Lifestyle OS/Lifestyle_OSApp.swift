ed3e1787bbbe042d097273cf59b8b6486d910e15
duyenle1312 <lengomyduyen@gmail.com>
2025-12-23 14:45:44 +0200

    Initial Commit

---
+++ Lifestyle OS/Lifestyle_OSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lifestyle_OSApp.swift
+//  Lifestyle OS
+//
+//  Created by Le Ngo My Duyen on 23.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Lifestyle_OSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
